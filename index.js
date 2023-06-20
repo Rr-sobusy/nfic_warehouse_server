0caa8b5c81b0e37a247dc7af05a8cf3175ab6cff
unknown <rockph0029@gmail.com>
2023-06-20 16:33:03 +0800

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+//Parsing bodies, parse URL-encoded and JSON request bodies
+app.use(express.urlencoded({ extended: true }));
+//Cors
+const cors = require("cors");
+//Use cors
+app.use(cors());
+
+app.get('/sample', (req,res)=>{
+    res.send("sample")
+})
+
+//Start the server
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+  console.log(`Server running at port:${PORT}`);
+});
